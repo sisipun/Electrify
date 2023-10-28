2798b1d35d4fe32250fd7ba9c3c6f2b2b1e74221
sisipun <sisipun@gmail.com>
2023-10-28 14:17:16 +0200

    Building boolean active change to int power to active stat + add station power stat

--- script/level/build/building.gd
+++ script/level/build/building.gd
@@ -5,30 +5,47 @@ extends Area2D
 @export_node_path("AnimatedSprite2D") var _body_path: NodePath
 @export_node_path("AnimatedSprite2D") var _effect_path: NodePath
 
+@export_range(1, 10) var _power_to_active: int
+
 @onready var _body: AnimatedSprite2D = get_node(_body_path)
 @onready var _effect: AnimatedSprite2D = get_node(_effect_path)
 
-var _active: bool
+var _power: int
 
 
 func _ready() -> void:
-	deactivate()
-	show_default()
+	_power = 0
+	_show_inactive()
+	show_default_effect()
 
 
-func activate() -> void:
-	_active = true
-	_body.play("active")
+func is_active() -> bool:
+	return _power >= _power_to_active
 
 
-func deactivate() -> void:
-	_active = false
-	_body.play("default")
+func increase_power(value: int) -> void:
+	_power += value
+	if is_active():
+		_show_active()
 
 
-func show_preview() -> void:
+func decrease_power(value: int) -> void:
+	_power -= value
+	if not is_active():
+		_show_inactive()
+
+
+func show_preview_effect() -> void:
 	_effect.play("preview")
 
 
-func show_default() -> void:
+func show_default_effect() -> void:
 	_effect.play("default")
+
+
+func _show_active() -> void:
+	_body.play("active")
+
+
+func _show_inactive() -> void:
+	_body.play("default")
--- script/level/station/station.gd
+++ script/level/station/station.gd
@@ -4,6 +4,8 @@ extends Area2D
 
 @export_node_path("StationZone") var _zone_path: NodePath
 
+@export_range(1, 10) var _power: int
+
 @onready var _zone: StationZone = get_node(_zone_path)
 
 var buildings: Array[Building] = []
@@ -18,9 +20,9 @@ func init(zone_radius: float) -> void:
 
 func _on_building_entered(building: Building) -> void:
 	buildings.append(building)
-	building.activate()
+	building.increase_power(_power)
 
 
 func _on_building_exited(building: Building) -> void:
+	building.decrease_power(_power)
 	buildings.remove_at(buildings.find(building))
-	building.deactivate()
